346429c552b21207e876766a3576d1fbe06fcb9e
Pablo Quirino <pabloquirino17@gmail.com>
2025-11-20 21:11:02 -0300

    feat: add standalone page components and remove test spec file

---
+++ src/app/pages/portifolio/portifolio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portifolio',
+  imports: [],
+  templateUrl: './portifolio.component.html',
+  styleUrl: './portifolio.component.scss'
+})
+export class PortifolioComponent {
+
+}
---
+++ src/app/pages/tecnologias/tecnologias.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnologias',
+  imports: [],
+  templateUrl: './tecnologias.component.html',
+  styleUrl: './tecnologias.component.scss'
+})
+export class TecnologiasComponent {
+
+}

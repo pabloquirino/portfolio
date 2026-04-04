d875b257fbc1e31f4259a8b333b187df51b3d4c3
Pablo Quirino <pabloquirino17@gmail.com>
2026-04-04 17:41:22 -0300

    Refactor code structure for improved readability and maintainability

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 export class SobreComponent {
   downloadCV() {
     const link = document.createElement('a')
-    link.href = '/assets/cv/CV_Pablo_Quirino.pdf'
+    link.href = '/assets/cv/cv_pablo_quirino.pdf'
     link.download = 'cv_pablo_quirino.pdf'
     link.click()
   }

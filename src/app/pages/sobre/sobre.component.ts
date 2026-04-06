2682848a9096f2b4a4de2d00a9f546b7bc845ea2
Pablo Quirino <pabloquirino17@gmail.com>
2026-04-06 16:21:01 -0300

    fix: atualizar link do CV para o novo arquivo motivacional.pdf

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 export class SobreComponent {
   downloadCV() {
     const link = document.createElement('a')
-    link.href = '/assets/cv/cv_pablo_quirino.pdf'
+    link.href = '/assets/cv/motivacional.pdf'
     link.download = 'cv_pablo_quirino.pdf'
     link.click()
   }
